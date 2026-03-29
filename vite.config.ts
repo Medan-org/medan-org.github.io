1b2a90edeceeadad4e6cc7efed16188f12ea12f5
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-29 19:23:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/Medan/",
+  base: "/",
   plugins: [react()],
   publicDir: "public"
 })
